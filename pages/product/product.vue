7462a002c3e27d4883cadba2b3f088e3bef1aec3
DESKTOP-8RJFHS5\Administrator <248550361@qq.com>
2023-12-16 00:36:51 +0800

    add

--- pages/product/product.vue
+++ pages/product/product.vue
@@ -1,65 +1,65 @@
 <template>
-	<view
-		style="background-image: url('../static/images/hm_bg.png');background-repeat: no-repeat;background-size: 100%;height: 100vh;">
-		<view style="padding:30rpx;">
-			<view class="u-flex">
-				<view style="margin-left: 10rpx;width:600rpx;">
-					<u-search bg-color="#FFFFFF" :placeholder="$t('enterFund')" :disabled="false" :show-action="false">
-					</u-search>
-				</view>
-				<view>
-					<image style="width:48rpx;height:48rpx;position: relative;margin-top:10rpx;margin-left: 30rpx;"
-						src="../../static/images/hm1.png"></image>
-				</view>
-			</view>
-			<view style="margin-top: 30rpx;">
-				<u-subsection v-if="forceRefresh" :list="tabList" @change="sectionChange" mode="button"
-					button-color="#E6F9F4" active-color="#35CBA5" bgColor="#ffffff" :current="current"></u-subsection>
-			</view>
-			<view
-				style="margin-top: 30rpx;background-color: #ffffff;box-shadow: 0px 0px 24rpx 0px rgba(0,0,0,0.05); padding: 30rpx;border-radius: 12rpx;"
-				v-for="(item,index) in stockProductListTab" :key="index" @click="toDetails(item)">
-				<view>{{item.product_name_cn}}</view>
-				<view class="u-flex" style="font-size: 24rpx;color: #AFAFAF;margin-top: 10rpx;">
-					<view style="width: 300rpx;">收益率：<text
-							style="color: #21BF90;">{{Number(item.interest_rate * 100)}}%</text></view>
-					<view style="width: 300rpx;" class="u-text-right">限购次数：<text
-							style="color: #21BF90;">{{item.limit_purchase_times}} 次</text>
-					</view>
-				</view>
-				<view class="u-flex" style="font-size: 24rpx;color: #AFAFAF;margin-top: 10rpx;">
-					<view style="width: 300rpx;">最低购买限额：<text
-							style="color: #21BF90;">{{item.min_purchase_amount}}U</text></view>
-					<view style="width: 300rpx;" class="u-text-right">最高购买限额：<text
-							style="color: #21BF90;">{{item.max_purchase_amount}}U</text>
-					</view>
-				</view>
-				<view class="u-flex" style="font-size: 24rpx;color: #AFAFAF;margin-top: 10rpx;">
-					<view style="width: 300rpx;">已售出份额：<text
-							style="color: #21BF90;">{{item.sold_units}}U</text></view>
-					<view style="width: 300rpx;" class="u-text-right">募资总额	：<text
-							style="color: #21BF90;">{{item.total_fundraising}}U</text>
-					</view>
-				</view>
-				<view style="width:540rpx;margin-top: 6rpx;">
-					<u-line-progress active-color="#6BEEAB" height="22" :percent="Number(item.progress)">
-					</u-line-progress>
-				</view>
-				<view class="u-flex" style="margin-top: 20rpx;">
-					<view style="background-color:#E1F7F2;color: #666666;font-size: 24rpx;
-						border-radius: 8rpx;padding-left:10rpx;padding-right:30rpx;">
-						<u-icon style="margin-right: 10rpx;" name="clock" color="#7ADDC4" size="24"></u-icon>
-						投资周期{{item.investment_period}}天
-					</view>
-					<view style="margin-left:200rpx;">
-						<u-button ripple-bg-color="#fff" @click="reg(item)" :ripple="true" style="font-size:28rpx;"
-							:custom-style="customStyle1" shape="circle" type="primary">{{$t('buyNow')}}</u-button>
-					</view>
-				</view>
-			</view>
-		</view>
-	</view>
+    <view style="background-image: url('../static/images/hm_bg.png');background-repeat: no-repeat;background-size: 100%;height: 100vh;">
+        <view style="padding:30rpx;">
+            <view class="u-flex">
+                <view style="margin-left: 10rpx;width:600rpx;">
+                    <u-search bg-color="#FFFFFF" :placeholder="$t('enterFund')" :disabled="false" :show-action="false">
+                    </u-search>
+                </view>
+                <view>
+                    <image style="width:48rpx;height:48rpx;position: relative;margin-top:10rpx;margin-left: 30rpx;"
+                        src="../../static/images/hm1.png"></image>
+                </view>
+            </view>
+            <view style="margin-top: 30rpx;">
+                <u-subsection v-if="forceRefresh" :list="tabList" @change="sectionChange" mode="button"
+                    button-color="#E6F9F4" active-color="#35CBA5" bgColor="#ffffff" :current="current"></u-subsection>
+            </view>
+            <view
+                style="margin-top: 30rpx;background-color: #ffffff;box-shadow: 0px 0px 24rpx 0px rgba(0,0,0,0.05); padding: 30rpx;border-radius: 12rpx;"
+                v-for="(item,index) in stockProductListTab" :key="index" @click="toDetails(item)">
+                <view>{{item.product_name_cn}}</view>
+                <view class="u-flex" style="font-size: 24rpx;color: #AFAFAF;margin-top: 10rpx;">
+                    <view style="width: 300rpx;">{{ $t('yieldRate') }}：<text
+                            style="color: #21BF90;">{{Number(item.interest_rate * 100)}}%</text></view>
+                    <view style="width: 300rpx;" class="u-text-right">{{ $t('purchaseLimit') }}：<text
+                            style="color: #21BF90;">{{item.limit_purchase_times}} {{$t('times')}}</text>
+                    </view>
+                </view>
+                <view class="u-flex" style="font-size: 24rpx;color: #AFAFAF;margin-top: 10rpx;">
+                    <view style="width: 300rpx;">{{ $t('minPurchase') }}：<text
+                            style="color: #21BF90;">{{item.min_purchase_amount}}U</text></view>
+                    <view style="width: 300rpx;" class="u-text-right">{{ $t('maxPurchase') }}：<text
+                            style="color: #21BF90;">{{item.max_purchase_amount}}U</text>
+                    </view>
+                </view>
+                <view class="u-flex" style="font-size: 24rpx;color: #AFAFAF;margin-top: 10rpx;">
+                    <view style="width: 300rpx;">{{ $t('soldUnits') }}：<text
+                            style="color: #21BF90;">{{item.sold_units}}U</text></view>
+                    <view style="width: 300rpx;" class="u-text-right">{{ $t('totalFundraising') }}：<text
+                            style="color: #21BF90;">{{item.total_fundraising}}U</text>
+                    </view>
+                </view>
+                <view style="width:540rpx;margin-top: 6rpx;">
+                    <u-line-progress active-color="#6BEEAB" height="22" :percent="Number(item.progress)">
+                    </u-line-progress>
+                </view>
+                <view class="u-flex" style="margin-top: 20rpx;">
+                    <view style="background-color:#E1F7F2;color: #666666;font-size: 24rpx;
+                        border-radius: 8rpx;padding-left:10rpx;padding-right:30rpx;">
+                        <u-icon style="margin-right: 10rpx;" name="clock" color="#7ADDC4" size="24"></u-icon>
+                        {{$t('investmentCycle')}}{{item.investment_period}}{{$t('days')}}
+                    </view>
+                    <view style="margin-left:200rpx;">
+                        <u-button ripple-bg-color="#fff" @click="reg(item)" :ripple="true" style="font-size:28rpx;"
+                            :custom-style="customStyle1" shape="circle" type="primary">{{$t('buyNow')}}</u-button>
+                    </view>
+                </view>
+            </view>
+        </view>
+    </view>
 </template>
+
 
 <script>
 	import {
