dc60014b3e52523d23f39285b40aa600c1dd289b
DESKTOP-8RJFHS5\Administrator <248550361@qq.com>
2023-12-17 13:32:26 +0800

    add

--- request/request.js
+++ request/request.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://api.broadreachvip.top/api' // 测试
-const BASE_URL = 'https://api.broadreachvip.top/api' // 正式
+// const BASE_URL = 'https://xingu.bianceok.info/api' // 测试
+const BASE_URL = 'https://xingu.bianceok.info/api' // 正式
 // const BASE_URL = '/api'
 import webUrl from '@/common/js/url.js'
 import Vue from 'vue'
