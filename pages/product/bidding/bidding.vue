49bdf39f824c2212ef64bedc49253d4bfd33f43b
DESKTOP-8RJFHS5\Administrator <248550361@qq.com>
2023-12-18 20:37:16 +0800

    add

--- pages/product/bidding/bidding.vue
+++ pages/product/bidding/bidding.vue
@@ -9,16 +9,21 @@
 					<view class="u-text-right" style="width:280rpx;margin-left:60rpx;">{{$t('investableAmount')}}</view>
 				</view>
 				<view class="u-flex" style="margin-top: 30rpx;font-size: 34rpx;">
-					<view style="width:280rpx;">¥ {{productInfo.total_units - productInfo.sold_units}}</view>
-					<view class="u-text-right" style="width:280rpx;margin-left:60rpx;">¥ {{productInfo.total_units}}</view>
+					<view style="width:280rpx;"> {{productInfo.total_units - productInfo.sold_units}}</view>
+					<view class="u-text-right" style="width:280rpx;margin-left:60rpx;"> {{productInfo.total_units}}</view>
 					</view>
 		</view>
 			<view style="background-color: #FFFFFF;border-radius: 12rpx;padding: 30rpx;margin-top: 30rpx;">
-				<view class="u-flex" style="margin-bottom:30rpx;">
+				  <view class="u-flex" style="margin-bottom:30rpx;">
 					<view style="width:200rpx;color: #666666;">{{$t('qtAmount')}}</view>
-					<view class="u-text-right" style="width:360rpx;margin-left:60rpx;color: #333333;">¥<text
-							class="col35" style="margin-left:6rpx;margin-right: 6rpx;">{{productInfo.amount_per_unit}}</text>元</view>
-				</view>
+					<view class="u-text-right" style="width:360rpx;margin-left:60rpx;color: #333333;"><text
+						class="col35" style="margin-left:6rpx;margin-right: 6rpx;">{{productInfo.min_purchase_amount}}</text>{{$t('yuan')}}</view>
+				  </view>
+				  <view class="u-flex" style="margin-bottom:30rpx;">
+					<view style="width:200rpx;color: #666666;">{{$t('remainingPurchaseTimes')}}</view>
+					<view class="u-text-right" style="width:360rpx;margin-left:60rpx;color: #333333;"><text
+						class="col35" style="margin-left:6rpx;margin-right: 6rpx;">{{productInfo.remaining_purchase_times}}</text>{{$t('times')}}</view>
+				  </view>
 				<u-line color="#F3F3F3"></u-line>
 				<view class="u-flex" style="margin-bottom:30rpx;margin-top:30rpx;">
 					<view style="width:200rpx;color: #666666;">{{$t('jxTime')}}</view>
@@ -42,8 +47,8 @@
 <!-- 						<u-number-box color="#ffffff" :min="parseInt(Number(productInfo.amount_per_unit))" :step="parseInt(Number(productInfo.amount_per_unit))" bg-color="#35CBA5" v-model="value" @change="valChange">
 						</u-number-box> -->
 						<u-field
-							v-model="productInfo.amount_per_unit"
-							placeholder="购买金额"
+							v-model="productInfo.min_purchase_amount"
+							:placeholder="$t('tzAmount')"
 						>
 						</u-field>
 					</view>{{$t('yuan')}}
@@ -56,6 +61,7 @@
 						<text class="col35" style="margin-left:6rpx;margin-right: 6rpx;">{{productInfo.max_purchase_amount}}</text> {{$t('yuan')}}
 					</view>
 				</view>
+				
 <!-- 				<u-line color="#F3F3F3"></u-line>
 				<view class="u-flex" style="margin-top:30rpx;">
 					<view style="width:200rpx;color: #666666;">{{$t('payPassword')}}</view>
@@ -124,7 +130,7 @@
 			},
 			toInvest() {
 				uni.navigateTo({
-					url: `/pages/product/security/security?id=${this.productId}&amount=${this.investmentAmount}`
+					url: `/pages/product/security/security?id=${this.productId}&amount=${this.productInfo.min_purchase_amount}`
 				})
 			}
 		}
